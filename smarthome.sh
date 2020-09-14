26edad367845fb41a0715f33d467ffe0d5201365
shuangyangyu <52737675+shuangyangyu@users.noreply.github.com>
2020-09-14 15:54:56 +0800

    Update smarthome.sh

--- smarthome.sh
+++ smarthome.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # 设置基本工作路径
 WHORK_PATCH=$(cd `dirname $0`;pwd)
+LOCAL_IP=`ifconfig ens3 |grep inet| grep netmas|cut -d" " -f10`
 #添加mqtt用户名以及密码
 read -p '为将来的MQTT定义同户名:' MQTT_name
 read -p '为将来的MQTT定义密码:' MQTT_passwd
@@ -11,3 +12,4 @@ echo YOUT_PASSWD :$MQTT_passwd
 sed 's/admin/'$MQTT_name'/g' $WHORK_PATCH/dockerfile/dockerfile-mosqutto/dockerfile
 #sed 's/passwd/'$MQTT_passwd'/g' $WHORK_PATCH/dockerfile/dockerfile-mosqutto/dockerfile
 #sed 's/admin/$MQTT_passwd/g' ~/config
+echo $LOCAL_IP
